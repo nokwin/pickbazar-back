210a48445da43ca0477c176c6e80720570c4e0af
Dmytro Batarin <dmitry.bat@computools.com>
2020-09-25 16:29:57 +0300

    Customize get /categories

--- api/categories/controllers/categories.js
+++ api/categories/controllers/categories.js
@@ -5,4 +5,38 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  async find() {
+    const entities = await strapi.services.categories.find();
+    const sanitizedEntities = entities.map(entity => cleanCategory(sanitizeEntity(entity, { model: strapi.models.categories })));
+    const parentCategories = sanitizedEntities.filter(entity => entity.parentCategory === null);
+    return parentCategories.map((category) => {
+      const childCategories = sanitizedEntities.filter((entity) => {
+        if(entity.parentCategory) {
+          return entity.parentCategory.id === category.id;
+        }
+
+        return false;
+      }).map(childCategory => ({
+        ...childCategory,
+        parentCategory: undefined
+      }));
+
+      return {
+        ...category,
+        parentCategory: undefined,
+        childCategories,
+      }
+    })
+  }
+};
+
+const cleanCategory = category => ({
+  ...category,
+  created_by: undefined,
+  updated_by: undefined,
+  created_at: undefined,
+  updated_at: undefined
+})
