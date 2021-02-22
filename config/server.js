bb5e24a487f2196c9a7a8f7d4411312bcf3e8994
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2021-02-22 15:52:47 +0200

    changed port

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '705624bcd420cd2f44d1cbb83ec3fdf8'),
+      secret: env("ADMIN_JWT_SECRET", "705624bcd420cd2f44d1cbb83ec3fdf8"),
     },
   },
 });
